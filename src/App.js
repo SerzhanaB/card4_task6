405e30e2056beb062fd6022a3b1f66e4128d5152
Serzhana <buraevase@gmail.com>
2021-09-20 23:12:50 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { Employees: [] };
+    this.myRef=React.createRef();
+  }
+
+  componentDidMount() {
+    this.setState({
+      Employees: [
+        { surname: "Willson", name: "Lila", days: 5, rate: 1000 },
+        { surname: "Willson", name: "Luiz", days: 7, rate: 1000 },
+        { surname: "Willson", name: "Luiz", days: 3, rate: 1000 },
+        { surname: "Willson", name: "Luiz", days: 2, rate: 1000 },
+      ],
+    });
+  }
+
+  handleValue = () => {
+    let newValue=this.myRef.current.value;
+    //let value=this.state.Employees.days;
+    console.log(newValue)
+   // this.setState({days: newValue})
+   //this.myRef.current.value='';
+  };
+
+  render() {
+    const { Employees } = this.state;
+    return (
+      <div>
+        <table border="1">
+          <caption>Таблица расчета заработной платы сотрудников</caption>
+          <thead>
+            <tr>
+              <th>Фамилия</th>
+              <th>Имя</th>
+              <th>Количество дней</th>
+              <th>Измененнные дни</th>
+              <th>Ставка за день</th>
+              <th>Измененная ставка</th>
+              <th>Сумма</th>
+              <th>Измененная Сумма</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Employees.map((item, index) => {
+              return (
+                <tr key={index.toString()}>
+                  <td>{item.surname}</td>
+                  <td>{item.name}</td>
+                  <td>{item.days}</td>
+                  <td>
+                    <textarea
+                      type="text"
+                      ref={this.myRef} onChange={this.handleValue}
+                    >
+                     
+                    </textarea>
+                  </td>
+                  <td>{item.rate}</td>
+                  <td>
+                    <input type="text" value={item.rate}></input>
+                  </td>
+                  <td>{item.rate * item.days}</td>
+                  <td>{item.rate * item.days}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      
+      </div>
+    );
+  }
 }
 
-export default App;
+export default CComp;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Ccomp from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Ccomp />
   </React.StrictMode>,
   document.getElementById('root')
 );
