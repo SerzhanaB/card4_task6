bf1fcaadbe4875d804219073a832243785a7e886
Serzhana <buraevase@gmail.com>
2021-10-16 15:38:11 +0300

    full

--- src/App.js
+++ src/App.js
@@ -3,51 +3,53 @@ import React, { Component } from "react";
 class CComp extends Component {
   constructor(props) {
     super(props);
-    this.state = { Employees: [] };
-    this.handleValue = this.handleValue.bind(this);
-  }
-
-  componentDidMount() {
-    this.setState({
+        this.state= {
       Employees: [
-        { id: 0, surname: "Willson", name: "Lila", days: 5, rate: 1000 },
-        { id: 1, surname: "Willson", name: "Luiz", days: 7, rate: 1000 },
-        { id: 2, surname: "Willson", name: "Luiz", days: 3, rate: 1000 },
-        { id: 3, surname: "Willson", name: "Luiz", days: 2, rate: 1000 },
+        { id: 0, surname: "Willson", name: "Lila", days: 5, rate: 1000 , sumid:5000},
+        { id: 1, surname: "Willson", name: "Luiz", days: 7, rate: 1000 , sumid:7000},
+        { id: 2, surname: "Willson", name: "Luiz", days: 3, rate: 1000 , sumid:3000},
+        { id: 3, surname: "Willson", name: "Luiz", days: 2, rate: 1000 , sumid:2000},
       ],
-      summ: 5,
-    });
+      summ: 0,
+    };
+    this.handleValue = this.handleValue.bind(this);
+    this.handleValue1 = this.handleValue1.bind(this);
+
+
+  }
+  componentDidMount() {
+    let summN=0;
+    const copy = this.state.Employees.slice();
+    copy.forEach((w) => (summN += w.sumid));
+    this.setState({ summ: summN });
+    console.log(summN)
+    return copy;
   }
 
   handleValue = (id, event) => {
-     const a = +event.target.value;
-    console.log(a);
+    const a = +event.target.value;
+    let summN=0;
+  
     const copy = this.state.Employees.slice();
     const result = copy.map((el, i) => {
-      return i === id ? { ...el, days: a } : el;
-    });
+      return i === id ? { ...el, days: a,sumid:a*el.rate } : el;});
 
+    result.forEach((w) => (summN += w.sumid));
     this.setState({ Employees: result });
-    console.log(this.state.Employees);
-    console.log(" result");
-    console.log(result);
-
+    this.setState({ summ: summN });
     return result;
   };
 
   handleValue1 = (id, event) => {
-    //  const idx=index;
-    // console.log(idx);
+    let summN=0;
     const a = +event.target.value;
-    console.log(a);
     const copy = this.state.Employees.slice();
     const result = copy.map((el, i) => {
-      return i === id ? { ...el, rate: a } : el;
-    });
+      return i === id ? { ...el, rate: a,sumid:a*el.days } : el;});
+
+      result.forEach((w) => (summN += w.sumid));
+      this.setState({ summ: summN });
     this.setState({ Employees: result });
-    console.log(this.state.Employees);
-    console.log(" result");
-    console.log(result);
     return result;
   };
 
@@ -77,19 +79,14 @@ class CComp extends Component {
                   <td>{item.name}</td>
                   <td>{item.days}</td>
                   <td>
-                    <textarea
-                      type="text"
-                      onChange={(e) => this.handleValue(item.id, e)}
-                    ></textarea>
+                    <textarea type="text" onChange={(e) => this.handleValue(item.id, e)}></textarea>
                   </td>
                   <td>{item.rate}</td>
                   <td>
-                    <input
-                      type="text"
-                      onChange={(e) => this.handleValue1(item.id, e)}
+                    <input type="text" onChange={(e) => this.handleValue1(item.id, e)}
                     ></input>
                   </td>
-                  <td>{item.rate * item.days}</td>
+                  <td>{item.sumid}</td>
                
                 </tr>
               );
